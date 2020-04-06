96f21c41cb775320265d6b3f6a5b621f99aa444b
Haresh Prajapati <haresh.p@masterlysolutions.com>
2020-04-06 23:25:39 +0530

    Added ViewChild() directive.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,7 +9,9 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // tslint:disable-next-line:no-output-rename
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  @ViewChild('inputServerName', {static: true}) serverInput: ElementRef;
+
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -18,11 +20,12 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(inputServerName: HTMLInputElement) {
-    this.serverCreated.emit({serverName: inputServerName.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName: this.serverInput.nativeElement.value, serverContent: this.newServerContent});
  }
 
 onAddBlueprint() {
-  this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  // this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  // this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
  }
 
 }
